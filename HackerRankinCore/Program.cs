0d9535a06e25a5c784284f50b76f3acfc491d1d7
[MarcusQ] <marcusquigley@gmail.com>
2019-02-23 17:37:02 -0500

    Add project files.

---
+++ HackerRankinCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankinCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
