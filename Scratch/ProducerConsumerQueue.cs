70ff3eaafe2c6e1ed664a2073bf346a1f83560e4
marcus quigley <marcusquigley@gmail.com>
2019-11-07 20:27:27 -0500

    adding scratch proj

---
+++ Scratch/ProducerConsumerQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Scratch
+{
+    public class ProducerConsumerQueue : IDisposable
+    {
+        //Queue<Action> queue = new Queue<Action>();
+        //ManualResetEvent signal = new ManualResetEvent(false);
+        EventWaitHandle _wh = new AutoResetEvent(false);
+        Thread _worker;
+        readonly object _locker = new object();
+        Queue<string> _tasks = new Queue<string>();
+
+        public ProducerConsumerQueue()
+        {
+            _worker = new Thread(Work);
+            _worker.Start();
+        }
+        public void Add(string action)
+        {
+            if (action != null)
+            {
+                _tasks.Enqueue(action);
+            }
+        }
+
+        void Work()
+        {
+            while (true)
+            {
+
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
